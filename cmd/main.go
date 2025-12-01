ff2f65d29f90921d65e6f71c3c62f51ce2c9f002
gurza <760903+gurza@users.noreply.github.com>
2025-12-01 19:46:21 +0300

    feat: solve day 1 part 1
    - refactor main to read input from stdin
    - add Parse() to convert lines into step values
    - solve part 1

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,32 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 
-	day01 "github.com/gurza/aoc2025/internal/day1"
+	"github.com/gurza/aoc2025/internal/day1"
 )
 
 func main() {
 	day := flag.Int("day", 1, "Which Advent of Code 2025 day to run")
 	flag.Parse()
 
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+		os.Exit(1)
+	}
+	lines := strings.Split(string(input), "\n")
+	if len(lines) > 0 && lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+
 	switch *day {
 	case 1:
-		day01.Solve()
+		data := day1.Parse(lines)
+		fmt.Printf("Part 1: %d\n", day1.Part1(data))
 	default:
-		fmt.Printf("Day %d not implemented yet.\n", *day)
+		fmt.Fprintf(os.Stderr, "Day %d not implemented yet.\n", *day)
 	}
 }
--- internal/day1/solution.go
+++ internal/day1/solution.go
@@ -1,3 +1,32 @@
-package day01
+package day1
 
-func Solve() {}
+import (
+	"strconv"
+)
+
+func Parse(input []string) []int {
+	res := make([]int, len(input))
+	for i, s := range input {
+		n, _ := strconv.Atoi(s[1:])
+		if s[0] == 'L' {
+			res[i] = -n
+		} else {
+			res[i] = n
+		}
+	}
+	return res
+}
+
+func Part1(steps []int) int {
+	pos := 50
+	pwd := 0
+
+	for _, step := range steps {
+		pos += step
+		if pos%100 == 0 {
+			pwd += 1
+		}
+	}
+
+	return pwd
+}
