7ddeee649a3c29d0ed98e6a9ece9cfd20f432694
gurza <760903+gurza@users.noreply.github.com>
2025-12-02 10:59:02 +0300

    feat: solve day 2 part 2

--- internal/day2/solution.go
+++ internal/day2/solution.go
@@ -5,8 +5,13 @@ import (
 	"strings"
 )
 
-func Parse(input []string) uint64 {
-	var sum uint64
+type Input struct {
+	p1 uint64
+	p2 uint64
+}
+
+func Parse(input []string) Input {
+	var sum1, sum2 uint64
 
 	for r := range strings.SplitSeq(input[0], ",") {
 		pp := strings.Split(r, "-")
@@ -14,25 +19,37 @@ func Parse(input []string) uint64 {
 		to, _ := strconv.Atoi(pp[1])
 
 		for num := from; num <= to; num++ {
-			if isInvalid(num) {
-				sum += uint64(num)
+			s := strconv.Itoa(num)
+			len := len(s)
+
+			half := len / 2
+			if s[:half] == s[half:] {
+				sum1 += uint64(num)
+				sum2 += uint64(num)
+				continue
+			}
+
+			for lenU := 1; lenU <= len/2; lenU++ {
+				if len%lenU != 0 {
+					continue
+				}
+				unit := s[:lenU]
+				repeated := strings.Repeat(unit, len/lenU)
+				if s == repeated {
+					sum2 += uint64(num)
+					break
+				}
 			}
 		}
 	}
 
-	return sum
+	return Input{sum1, sum2}
 }
 
-func isInvalid(n int) bool {
-	s := strconv.Itoa(n)
-	l := len(s)
-	if l%2 != 0 {
-		return false
-	}
-	half := l / 2
-	return s[:half] == s[half:]
+func Part1(input Input) uint64 {
+	return input.p1
 }
 
-func Part1(p1 uint64) uint64 {
-	return p1
+func Part2(input Input) uint64 {
+	return input.p2
 }
