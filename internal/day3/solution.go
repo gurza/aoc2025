7f5831a2f67f917a9e1223285ae0a5f463f9720a
gurza <760903+gurza@users.noreply.github.com>
2025-12-03 17:40:37 +0300

    feat: solve day 3 part 2

--- internal/day3/solution.go
+++ internal/day3/solution.go
@@ -6,27 +6,61 @@ type Input struct {
 }
 
 func Parse(input []string) Input {
+	const n = 12
 	var sum1, sum2 uint64
 
 	for _, s := range input {
-		left := s[0] - '0'
-		max := uint64(0)
+		sLen := len(s)
 
-		for i := 1; i < len(s); i++ {
+		// for two batteries
+		var max2 uint64
+		left2 := s[0] - '0'
+
+		// for n batteries
+		var stack [n]uint8
+		stackPos := 0
+		toDrop := sLen - n
+
+		// single pass over s
+		for i := range sLen {
 			cur := s[i] - '0'
-			cand := uint64(10*left + cur)
-			if cand > max {
-				max = cand
+
+			// n batteries
+			for stackPos > 0 && stack[stackPos-1] < cur && toDrop > 0 {
+				stackPos--
+				toDrop--
 			}
-			if cur > left {
-				left = cur
+			if stackPos < n {
+				stack[stackPos] = cur
+				stackPos++
+			} else {
+				toDrop--
+			}
+
+			// two batteries
+			cand := uint64(10*left2 + cur)
+			if cand > max2 {
+				max2 = cand
+			}
+			if cur > left2 {
+				left2 = cur
 			}
 		}
 
-		sum1 += max
+		sum1 += max2
+		sum2 += stackToNumber(stack[:])
 	}
 
 	return Input{sum1, sum2}
+}
+
+func stackToNumber(stack []uint8) uint64 {
+	var res uint64
+	n := len(stack)
+	for i := range n {
+		res = res*10 + uint64(stack[i])
+	}
+	return res
 }
 
 func Part1(input Input) uint64 {
